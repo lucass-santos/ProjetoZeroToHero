149731a08399bc822e5407153968c62c90d0e159
Lucas Santos de Oliveira <lucas.ramuh@gmail.com>
2023-09-12 10:49:55 -0300

    Teste dos metodos getAll e getById

--- product-ms/src/test/java/br/com/youtube/productms/service/ProductServiceTest.java
+++ product-ms/src/test/java/br/com/youtube/productms/service/ProductServiceTest.java
@@ -3,16 +3,22 @@ package br.com.youtube.productms.service;
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 import br.com.youtube.productms.dto.ProductDTO;
+import br.com.youtube.productms.model.Product;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest
+@Sql(executionPhase = BEFORE_TEST_METHOD, scripts = {"classpath:clear-database.sql"})
 public class ProductServiceTest {
 
     @Autowired
@@ -32,6 +38,36 @@ public class ProductServiceTest {
         assertEquals(request.getDescription(), response.get().getDescription());
         assertEquals(request.getPrice(), response.get().getPrice());
         assertTrue(response.get().isAvailable());
+
+    }
+
+    @Test
+    public void  shouldGetAllProducts(){
+        ProductDTO request = Fixture.from(ProductDTO.class).gimme("valid");
+        Optional<ProductDTO> response = service.create(request);
+        List<ProductDTO> responses = service.getAll();
+        System.out.println(">>>>>>>>>>>" + responses.size());
+        assertNotNull(responses);
+        assertEquals(responses.get(0).getName(), response.get().getName());
+        assertEquals(responses.get(0).getDescription(), response.get().getDescription());
+        assertEquals(responses.get(0).getPrice(), response.get().getPrice());
+
+    }
+
+    @Test
+    public void shouldGetByIdProduct(){
+        ProductDTO request = Fixture.from(ProductDTO.class).gimme("valid");
+        Optional<ProductDTO> response = service.create(request);
+
+        Long id = response.get().getId();
+
+        Optional<ProductDTO> responseById = service.getById(id);
+
+        assertNotNull(responseById.get());
+        assertEquals(request.getName(), responseById.get().getName());
+        assertEquals(request.getDescription(), responseById.get().getDescription());
+        assertEquals(request.getPrice(), responseById.get().getPrice());
+        assertTrue(responseById.get().isAvailable());
 
     }
 
